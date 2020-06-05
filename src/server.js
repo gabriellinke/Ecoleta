c521cc97a0fb6161aa1c4515fc42577c1969a5a7
Linke <51447706+gabriellinke@users.noreply.github.com>
2020-06-05 09:09:36 -0300

    Aula 4

--- src/server.js
+++ src/server.js
@@ -1,5 +1,35 @@
 const express = require("express")
 const server = express()
 
+// Configurar pasta pública
+server.use(express.static("public"))
+
+
+// Utilizando template engine
+// Enquanto estiver desenvolvendo deixa sem cache, para nao ter problemas
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
+// Configurar caminhos da minhas aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create_point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 // ligar o servidor
 server.listen(3000)
